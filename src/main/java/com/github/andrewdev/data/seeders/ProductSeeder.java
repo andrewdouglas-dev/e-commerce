5fec15360a7c62204395f0481b8580f1c485ea53
andrewdouglas-dev <andrew.r.douglas@outlook.com>
2026-03-20 15:43:02 -0500

    Updated Seeder to use BigDecimal rather than double for Price values.

--- src/main/java/com/github/andrewdev/data/seeders/ProductSeeder.java
+++ src/main/java/com/github/andrewdev/data/seeders/ProductSeeder.java
@@ -1,5 +1,7 @@
 package com.github.andrewdev.data.seeders;
 
+import java.math.BigDecimal;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.github.andrewdev.data.dao.ProductDAO;
@@ -21,15 +23,15 @@ public class ProductSeeder {
         for (int i = 0; i < TOTAL_PRODUCTS; i++) {
             Product p = new Product(
                 faker.commerce().productName(), 
-                Double.parseDouble(faker.commerce().price()),
+                BigDecimal.valueOf(Double.parseDouble(faker.commerce().price())),
                 faker.number().numberBetween(0, 1000)
             );
 
             productDAO.add(p);
 
-            logger.info("Insert Record (" + i + "), name: " + p.getName() + " price: " + p.getPrice() + " quantity: " + p.getQuantity());
+            logger.log(Level.INFO, "Insert Record ({0}), name: {1} price: {2} quantity: {3}", new Object[]{i, p.getName(), p.getPrice(), p.getQuantity()});
         }
 
-        logger.info("Successfully seeded " + TOTAL_PRODUCTS + " products.");
+        logger.log(Level.INFO, "Successfully seeded {0} products.", TOTAL_PRODUCTS);
     }
 }
